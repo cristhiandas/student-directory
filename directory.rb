feba5b65604e6ab979fcfdef6ae8b26991b8d947
cristhiandas <cristhiandf94@gmail.com>
2017-10-11 16:20:28 +0000

    Changed the structure to Hashes

--- directory.rb
+++ directory.rb
@@ -1,17 +1,17 @@
 #we have the students in an array
 students = [
-["Dr. Hannibal Lecter", :november],
-["Darth Vader", :november],
-["Nurse Ratched", :november],
-["Michael Corleone", :november],
-["Alex DeLarge", :november],
-["The Wicked Witch of the West", :november],
-["Terminator", :november],
-["Freddy Krueger", :november],
-["The Joker", :november],
-["Joffrey Baratheon", :november],
-["Norman Bates", :november],
-["The one that putted HL and The Joker as villians, they are true heroes", :november]]
+{name: "Dr. Hannibal Lecter",cohort :november},
+{name: "Darth Vader",cohort :november},
+{name: "Nurse Ratched",cohort :november},
+{name: "Michael Corleone",cohort :november},
+{name: "Alex DeLarge",cohort :november},
+{name: "The Wicked Witch of the West",cohort :november},
+{name: "Terminator",cohort :november},
+{name: "Freddy Krueger",cohort :november},
+{name: "The Joker",cohort :november},
+{name: "Joffrey Baratheon",cohort :november},
+{name: "Norman Bates",cohort :november},
+{name: "The one that putted HL and The Joker as villians, they are true heroes",cohort :november}]
 #We create a head-printer method
 
 def print_head
@@ -22,7 +22,7 @@ end
 #Now an students-printer method
 def print_students(names)
   names.each do |name|
-    puts "#{name[0]} (#{name[1]} cohort)"
+    puts "#{name[:name]} (#{name[:cohort]} cohort)"
   end
 end
 
