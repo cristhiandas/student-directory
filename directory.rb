0d1ae3e22d63752932b4a262a14d34c83b5c681c
cristhiandas <cristhiandf94@gmail.com>
2017-10-11 15:50:36 +0000

    More effcient code with arrays baby

--- directory.rb
+++ directory.rb
@@ -1,19 +1,22 @@
-# First is we have the students
+#we have the students in an array
+students = [
+"Dr. Hannibal Lecter",
+"Darth Vader",
+"Nurse Ratched",
+"Michael Corleone",
+"Alex DeLarge",
+"The Wicked Witch of the West",
+"Terminator",
+"Freddy Krueger",
+"The Joker",
+"Joffrey Baratheon",
+"Norman Bates",
+"The one that putted HL and The Joker as villians, they are true heroes"]
+#Now, we print it
 puts "The Students of The Villian Academy"
 puts "----------------------------"
-puts "Dr. Hannibal Lecter"
-puts "Darth Vader"
-puts "Nurse Ratched"
-puts "Michael Corleone"
-puts "Alex DeLarge"
-puts "The Wicked Witch of the West"
-puts "Terminator"
-puts "Freddy Krueger"
-puts "The Joker"
-puts "Joffrey Baratheon"
-puts "Norman Bates"
-puts "The one that putted HL and The Joker as villians, they are true heroes"
-#Then, we have the students count
-student_count = 12
+students.each do |student|
+  puts student
+end
 #Now we print the sum of the students
-print "Overall, we have #{student_count} great students"
+print "Overall, we have #{students.count} great students"
