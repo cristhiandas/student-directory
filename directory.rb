11558d7a401ec627d3cc6d00fb7e4515bf3c58cb
cristhiandas <cristhiandf94@gmail.com>
2017-10-11 15:39:00 +0000

    Refractoring my code + fixing a typo

--- directory.rb
+++ directory.rb
@@ -13,7 +13,7 @@ puts "The Joker"
 puts "Joffrey Baratheon"
 puts "Norman Bates"
 puts "The one that putted HL and The Joker as villians, they are true heroes"
+#Then, we have the students count
+student_count = 12
 #Now we print the sum of the students
-print "Over all, we have "
-print 12
-print " great students"
+print "Overall, we have #{student_count} great students"
