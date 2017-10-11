d0e2576d3dc56990301ba29149bbe279f30d50b7
cristhiandas <cristhiandf94@gmail.com>
2017-10-11 15:35:41 +0000

    Added sum of students and coments

--- directory.rb
+++ directory.rb
@@ -1,3 +1,4 @@
+# First is we have the students
 puts "The Students of The Villian Academy"
 puts "----------------------------"
 puts "Dr. Hannibal Lecter"
@@ -12,3 +13,7 @@ puts "The Joker"
 puts "Joffrey Baratheon"
 puts "Norman Bates"
 puts "The one that putted HL and The Joker as villians, they are true heroes"
+#Now we print the sum of the students
+print "Over all, we have "
+print 12
+print " great students"
