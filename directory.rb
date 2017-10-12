ed708983465461157a5e5d69ba355fdc92dce831
cristhiandas <cristhiandf94@gmail.com>
2017-10-12 12:52:51 +0000

    Now it sorts the students and the user can choose to see only one cohort or all the cohorts in the display (I wasn't sure which one you want, so why not both?)

--- directory.rb
+++ directory.rb
@@ -1,22 +1,8 @@
-#I've created a class method that will return the initial of any word (ty stackoverflow)
-class String
-  def initial
-    self[0,1]
-  end
-end
 #We create a head-printer method
 def print_head
   puts "The Students of The Villian Academy".center(80)
   puts "----------------------------".center(80)
 end
-
-
-#We print the studentes name
-# def print_students(names)
-#   names.each do |name|
-#      puts "#{name[:name]} (#{name[:cohort]} cohort)".center(80)
-#    end
-#  end
 
 #Now the print the sum of the students method
 def print_footer(names)
@@ -26,61 +12,6 @@ def print_footer(names)
     puts "Overall, we have #{names.count} great students \n".center(80)
   end
 end
-
-#This method sort the array of students based on the cohort
-def sort_cohort(choosen_cohort, names)
-  # sorted_cohorts = []
-  choosen_cohort.map do |month|
-    selector = 0
-      names.each do |student|
-        if student[:cohort] == month && selector == 1
-          # sorted_cohorts << student
-          puts student[:name]
-        elsif student[:cohort] == month && selector == 0
-          selector = 1
-          puts "The students of the #{month} cohort are:"
-          puts student[:name]
-        end
-      end
-
-  end
-  # print_students(sorted_cohorts)
-end
-
-
-
-#This method will ask for a cohort to select, if empty will select november by default, if it have a typo
-#It will show a wrong input message
-
-
-def input_cohort(choosen_cohort)
-  puts "Now Choose the cohort, please write ONLY the number of the cohort you want".center(60)
-  puts "1 January       2 February        3 March         4 April            5 May         6 June".center(60)
-  puts "7 July          8 August        9 September      10 October      11 November   12 December".center(60)
-#We get the number of the cohort
-  cohort = gets.chomp
-  puts "You wrote #{cohort}, are you sure that's the cohort you want? Y/N"
-  choice = gets.chomp
-#If empty, returns the default cohort November
-  if choice.upcase == "Y"
-    if cohort.empty?
-      choosen_cohort[10]
-#If the number is selected correctly it will return the cohort
-    elsif 0 < cohort.to_i && cohort.to_i < 13
-      return choosen_cohort[cohort.to_i-1]
-    else
-#If the input is not in the list we have the wrong input message
-      puts "Wrong input, try again"
-      input_cohort(choosen_cohort)
-    end
-  elsif choice.upcase == "N"
-    input_cohort(choosen_cohort)
-  else
-    puts "wrong input try again"
-    input_cohort(choosen_cohort)
-  end
-end
-
 
 
 def input_students(choosen_cohort)
@@ -113,26 +44,102 @@ students
 
 end
 
+
+#This method will ask for a cohort to select, if empty will select november by default, if it have a typo
+#It will show a wrong input message
+
+
+def input_cohort(choosen_cohort)
+  puts "Choose the cohort, please write ONLY the number of the cohort you want".center(60)
+  puts "1 January       2 February        3 March         4 April            5 May         6 June".center(60)
+  puts "7 July          8 August        9 September      10 October      11 November   12 December".center(60)
+#We get the number of the cohort
+  cohort = gets.chomp
+  puts "You wrote #{cohort}, are you sure that's the cohort you want? Y/N"
+  choice = gets.chomp
+#If empty, returns the default cohort November
+  if choice.upcase == "Y"
+    if cohort.empty?
+      choosen_cohort[10]
+#If the number is selected correctly it will return the cohort
+    elsif 0 < cohort.to_i && cohort.to_i < 13
+      return choosen_cohort[cohort.to_i-1]
+    else
+#If the input is not in the list we have the wrong input message
+      puts "Wrong input, try again"
+      input_cohort(choosen_cohort)
+    end
+  elsif choice.upcase == "N"
+    input_cohort(choosen_cohort)
+  else
+    puts "wrong input try again"
+    input_cohort(choosen_cohort)
+  end
+end
+
+
+
+#This method sort the array of students based on the cohort
+def sort_cohort(choosen_cohort, names)
+#We now print the header here
+print_head
+
+  # sorted_cohorts = []
+  #It take the arrays of the months that are already sorted
+  choosen_cohort.map do |month|
+    #this selector will tell us if it is the first name on the list, in that case it will print the "header"
+    #of the list
+    first = true
+      names.each do |student|
+        if student[:cohort] == month && first == false
+          # sorted_cohorts << student
+          puts "#{student[:name]}".center(80)
+        elsif student[:cohort] == month && first == true
+          first = false
+          puts "The students of the #{month} cohort are:".center(80)
+          puts "#{student[:name]}".center(80)
+        end
+      end
+
+  end
+  # print_students(sorted_cohorts)
+end
+
+
+def cohort_display(choosen_cohort)
+  puts "Do you want to select which cohort is displayed? Y/N".center(60)
+  puts "If you don't, all of them will be displayed".center(60)
+  option = gets.chomp
+  if option.upcase == "Y"
+    return [input_cohort(choosen_cohort)]
+  elsif option.upcase == "N"
+    return choosen_cohort
+  else
+    puts "Wrong argument, try again"
+    cohort_display(choosen_cohort)
+  end
+end
+
+#This is the body of the program with the needed array
 def program
   #first we need an array for the cohorts, we want them to be symbols
-  choosen_cohort = [:January,
-  :February,
-  :March,
-  :April,
-  :May,
-  :June,
-  :July,
-  :August,
-  :September,
-  :October,
-  :November,
-  :December
-  ]
+    choosen_cohort = [:January,
+    :February,
+    :March,
+    :April,
+    :May,
+    :June,
+    :July,
+    :August,
+    :September,
+    :October,
+    :November,
+    :December
+    ]
 
-students = input_students(choosen_cohort)
-print_head
-sort_cohort(choosen_cohort, students)
-print_footer(students)
+  students = input_students(choosen_cohort)
+  sort_cohort(cohort_display(choosen_cohort), students)
+  print_footer(students)
 end
 
 program
