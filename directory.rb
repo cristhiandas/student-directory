0a5fca7a59e0b720c46b686c2e1f1cd2de6e5ba8
cristhiandas <cristhiandf94@gmail.com>
2017-10-12 11:43:01 +0000

    Now when we have 1 student it will use student instead of students in all the code (including the overall section), also I added a Y/N question after the cohort selection to be sure that the user don't make any mistakes since I'm working with numbers ;)

--- directory.rb
+++ directory.rb
@@ -13,6 +13,7 @@ end
 
 #We print the studentes name
 def print_students(names)
+  sort_cohort(names)
   names.each do |name|
      puts "#{name[:name]} (#{name[:cohort]} cohort)".center(80)
    end
@@ -20,11 +21,28 @@ def print_students(names)
 
 #Now the print the sum of the students method
 def print_footer(names)
+  if names.count ==1
+    puts "Overall, we have #{names.count} great student \n".center(80)
+  else
     puts "Overall, we have #{names.count} great students \n".center(80)
+  end
 end
+
+
+def sort_cohort(names)
+  names.map do |hash|
+    hash.each do |value1, value2|
+
+    end
+  end
+end
+
+
 
 #This method will ask for a cohort to select, if empty will select november by default, if it have a typo
 #It will show a wrong input message
+
+
 def input_cohort
 #first we need an array for the cohorts, we want them to be symbols
 choosen_cohort = [:January,
@@ -43,17 +61,30 @@ choosen_cohort = [:January,
   puts "Now Choose the cohort, please write ONLY the number of the cohort you want".center(60)
   puts "1 January       2 February        3 March         4 April            5 May         6 June".center(60)
   puts "7 July          8 August        9 September      10 October      11 November   12 December".center(60)
-#We get the text, we change it to Integer because it will have less
+#We get the number of the cohort
   cohort = gets.chomp
-  if cohort.empty?
-    return choosen_cohort[10]
-  elsif 0 < cohort.to_i && cohort.to_i < 13
-    return choosen_cohort[cohort.to_i-1]
+  puts "You wrote #{cohort}, are you sure that's the cohort you want? Y/N"
+  choice = gets.chomp
+#If empty, returns the default cohort November
+  if choice.upcase == "Y"
+    if cohort.empty?
+      choosen_cohort[10]
+#If the number is selected correctly it will return the cohort
+    elsif 0 < cohort.to_i && cohort.to_i < 13
+      return choosen_cohort[cohort.to_i-1]
+    else
+#If the input is not in the list we have the wrong input message
+      puts "Wrong input, try again"
+      input_cohort
+    end
+  elsif choice.upcase == "N"
+    input_cohort
   else
-    puts "Wrong input, try again"
+    puts "wrong input try again"
     input_cohort
   end
 end
+
 
 
 def input_students
@@ -69,9 +100,13 @@ def input_students
   #We need to repeat the code while the name isn't empty
   while !name.empty? do
 
-      students << {name: name, cohort: input_cohort}
-      puts "hello 6"
-    puts "Now we have #{students.count} students"
+    students << {name: name, cohort: input_cohort}
+
+    if students.count != 1
+      puts "Now we have #{students.count} students"
+    else
+      puts "Now we have #{students.count} student"
+    end
 
     #and we ask for names again
     puts "Now you can add another name or return to finish the program"
@@ -82,7 +117,11 @@ students
 
 end
 
+def program
 students = input_students
 print_head
 print_students(students)
 print_footer(students)
+end
+
+program
