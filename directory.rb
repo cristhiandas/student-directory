e3f60856f273d92aaa5596a19b00e960fa6dbf42
cristhiandas <cristhiandf94@gmail.com>
2017-10-12 10:45:33 +0000

    Now we can choose the cohort, if have the input error, a default value and it gets symbols, really sexy I tried to refract it as much as my mind could

--- directory.rb
+++ directory.rb
@@ -1,18 +1,3 @@
-#we have the students in an array
-# students = [
-# {name: "Dr. Hannibal Lecter",cohort :november},
-# {name: "Darth Vader",cohort :november},
-# {name: "Nurse Ratched",cohort :november},
-# {name: "Michael Corleone",cohort :november},
-# {name: "Alex DeLarge",cohort :november},
-# {name: "The Wicked Witch of the West",cohort :november},
-# {name: "Terminator",cohort :november},
-# {name: "Freddy Krueger",cohort :november},
-# {name: "The Joker",cohort :november},
-# {name: "Joffrey Baratheon",cohort :november},
-# {name: "Norman Bates",cohort :november},
-# {name: "The one that putted HL and The Joker as villians, they are true heroes",cohort :november}]
-
 #I've created a class method that will return the initial of any word (ty stackoverflow)
 class String
   def initial
@@ -25,35 +10,6 @@ def print_head
   puts "----------------------------".center(80)
 end
 
-#Now an students-printer method with index and the initial
-# def print_students(names)
-#   names.each_with_index do |name, index|
-#     puts "#{index+1}. #{name[:name]} (#{name[:cohort]} cohort)" if name[:name].initial.downcase == "m"
-#   end
-# end
-
-#We print the studentes name with less than 12 char
-# def print_students(names)
-#   names.each do |name|
-#     puts "#{name[:name]} (#{name[:cohort]} cohort)" if name[:name].length < 12
-#   end
-# end
-
-#We print the students names with a while loop instead of a each iterator
-# def print_students(names)
-#   index = 0
-#   while index < names.length
-#     puts "#{names[index][:name]} #{names[index][:cohort]}"
-#     index += 1
-#   end
-# end
-
-#We print the studentes name, hobbies, country_of_birth, etc
-# def print_students(names)
-#   names.each do |name|
-#      puts "#{name[:name]} \nHeight: #{name[:height]}\nHobbies: #{name[:hobbies]}\nCountry of Birth: #{name[:country_of_birth]}\nOther Info: #{name[:relevant_info]} \n(#{name[:cohort]} cohort)"
-#    end
-#  end
 
 #We print the studentes name
 def print_students(names)
@@ -67,6 +23,39 @@ def print_footer(names)
     puts "Overall, we have #{names.count} great students \n".center(80)
 end
 
+#This method will ask for a cohort to select, if empty will select november by default, if it have a typo
+#It will show a wrong input message
+def input_cohort
+#first we need an array for the cohorts, we want them to be symbols
+choosen_cohort = [:January,
+:February,
+:March,
+:April,
+:May,
+:June,
+:July,
+:August,
+:September,
+:October,
+:November,
+:December
+]
+  puts "Now Choose the cohort, please write ONLY the number of the cohort you want".center(60)
+  puts "1 January       2 February        3 March         4 April            5 May         6 June".center(60)
+  puts "7 July          8 August        9 September      10 October      11 November   12 December".center(60)
+#We get the text, we change it to Integer because it will have less
+  cohort = gets.chomp
+  if cohort.empty?
+    return choosen_cohort[10]
+  elsif 0 < cohort.to_i && cohort.to_i < 13
+    return choosen_cohort[cohort.to_i-1]
+  else
+    puts "Wrong input, try again"
+    input_cohort
+  end
+end
+
+
 def input_students
   #first we ask for the names
   puts "Please dear user, write the name of a student and press return"
@@ -79,19 +68,9 @@ def input_students
 
   #We need to repeat the code while the name isn't empty
   while !name.empty? do
-    #Now we add the Info
-    # puts "please, write the student's hobbies, if doesn't have any write N/A"
-    # hobbies = gets.chomp
-    # puts "please, write the student's country of birth"
-    # country_of_birth = gets.chomp
-    # puts "please, write the student's height"
-    # height = gets.chomp
-    # puts "please, write any other relevant info, if doesn't have any write N/A"
-    # relevant_info = gets.chomp
 
-    #We add the students to the array
-    # students << {name: name,hobbies: hobbies, country_of_birth: country_of_birth, height: height, relevant_info: relevant_info, cohort: :november}
-      students << {name: name, cohort: :november}
+      students << {name: name, cohort: input_cohort}
+      puts "hello 6"
     puts "Now we have #{students.count} students"
 
     #and we ask for names again
